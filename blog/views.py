4c51c03eb5e9631f4ca87561fbd11718f61fe3a8
almaziztay <sexmawin010@mail.ru>
2020-11-11 02:30:27 +0600

    site is already done

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 from .forms import PostForm, CommentForm
 from .models import Post, Comment
 
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
